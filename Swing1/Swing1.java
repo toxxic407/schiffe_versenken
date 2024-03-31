915d3b44b286568f96feeac22139436baea484be
toxxic407 <johenpaennle@gmail.com>
2024-03-31 17:57:14 +0100

    8x8 Field added and GUI Management improved

--- Swing1/Swing1.java
+++ Swing1/Swing1.java
@@ -14,8 +14,11 @@ import java.awt.*;
 public class Swing1 {
 	
 	static public JFrame f = new JFrame();
+	public static JPanel connection_panel = new JPanel();
+	public static JPanel side_panel = new JPanel();
 	public static JPanel method_panel = new JPanel();
 	public static boolean ipselector_is_shown = false;
+	public static boolean turn = true;
 	
 	public static void main(String[] args) throws IOException {
 		f.setLayout(new FlowLayout());
@@ -24,6 +27,8 @@ public class Swing1 {
 		f.setSize(1000,1000);
 		f.setVisible(true);
 		spawn_connection_selector();
+		spawn_field();
+		spawn_side_panel();
 		SwingUtilities.updateComponentTreeUI(f);
 	}
 	
@@ -45,13 +50,15 @@ public class Swing1 {
 	        			JLabel label = new JLabel("Select your server:");
 	        			JButton button=new JButton("Join");
 	       		
-	        			f.remove(method_panel);
+	        			connection_panel.remove(method_panel);
 	        			method_panel = new JPanel();
+	        			method_panel.setBackground(Color.green);
+	    				method_panel.setPreferredSize(new Dimension(500, 100));
 	       		
 	        			method_panel.add(label);
 	        			method_panel.add(selector);
 	        			method_panel.add(button);
-	        			f.add(method_panel);
+	        			connection_panel.add(method_panel);
 	        			SwingUtilities.updateComponentTreeUI(f);
 	        		} catch (Exception ignore) {
 	        		}
@@ -61,52 +68,58 @@ public class Swing1 {
 
 	   (new ipFinder()).execute();
 		   
-		method_panel = new JPanel();
 		JLabel wait = new JLabel("Waiting for Network Scan to finish...");
 		method_panel.add(wait);
-		f.add(method_panel);
 	
 	}
 	
 	public static void spawn_manual() throws IOException {
-		method_panel = new JPanel();
 		JLabel label = new JLabel("Enter server address:");
-		JTextField ip = new JTextField(16);
+		JTextField ip = new JTextField();
 		JButton button=new JButton("Join");//creating instance of JButton  
 		
 		method_panel.add(label);
 		method_panel.add(ip);
 		method_panel.add(button);
-		f.add(method_panel);
 	
 	}
 	
 	public static void spawn_host_info() throws IOException {
+		//Show IP
 		byte[] ip = InetAddress.getLocalHost().getAddress();
-		method_panel = new JPanel();
 		JLabel label = new JLabel("IP: "+ InetAddress.getByAddress(ip).toString().substring(1));
-		JButton button=new JButton("Start Game");//creating instance of JButton  
+		JButton button=new JButton("Start Game");
 		
 		method_panel.add(label);
 		method_panel.add(button);
-		f.add(method_panel);
 	
 	}
 	
 	public static void spawn_connection_selector() throws IOException {
+		
+		connection_panel.setLayout(new FlowLayout());
+		
 		String[] connectiontypes = {"Host","Select IP","Enter address manually"}; 
 		JPanel panel = new JPanel();
+		panel.setBackground(Color.blue);
+		panel.setPreferredSize(new Dimension(400, 100));
 		JComboBox method_selector = new JComboBox(connectiontypes);
 		JLabel label = new JLabel("Select your connection type:");
 		panel.add(label);
 		panel.add(method_selector);
-		f.add(panel);
+		connection_panel.add(panel);
 		
 		//spawn initial option
+		method_panel.setBackground(Color.green);
+		method_panel.setPreferredSize(new Dimension(400, 100));
 		spawn_host_info();
+		connection_panel.add(method_panel);
 		method_selector.addActionListener (new ActionListener () {
 		    public void actionPerformed(ActionEvent e) {
-		    	f.remove(method_panel);
+		    	connection_panel.remove(method_panel);
+		    	method_panel = new JPanel();
+		    	method_panel.setBackground(Color.green);
+				method_panel.setPreferredSize(new Dimension(500, 100));
 		    	try {
 		    		switch(method_selector.getSelectedItem().toString()) {
 		    			case "Host":
@@ -133,10 +146,12 @@ public class Swing1 {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-		    	
+		    	connection_panel.add(method_panel);
 		    	SwingUtilities.updateComponentTreeUI(f);
 		    }
 		});
+		
+		f.add(connection_panel);
 	
 	}
 	
@@ -178,6 +193,39 @@ public class Swing1 {
     }
 
 	public static void spawn_field() {
-		
+		JPanel grid_panel = new JPanel();
+		grid_panel.setLayout(new GridLayout(8,8));
+		JButton[] button = new JButton[64];
+		for(int i = 0; i<64;i++) {
+			JButton temp = new JButton(Integer.toString(i+1));
+			temp.setPreferredSize(new Dimension(80, 80));
+			button[i] = temp;
+			button[i].addActionListener(new ActionListener() {
+				@Override
+			    public void actionPerformed(ActionEvent e) {
+			        turn = !turn;
+			        f.remove(side_panel);
+			        spawn_side_panel();
+			    }
+			});
+			grid_panel.add(button[i]);
+		}
+		f.add(grid_panel);
+	}
+	
+	public static void spawn_side_panel() {
+		side_panel = new JPanel();
+		side_panel.setLayout(new FlowLayout());
+		JLabel turntext;
+		if(turn) {
+			turntext = new JLabel("Your Turn!");
+			turntext.setForeground(Color.green);
+		}else {
+			turntext = new JLabel("Opponents Turn!");
+			turntext.setForeground(Color.red);
+		}
+		side_panel.add(turntext);
+		f.add(side_panel);
+		SwingUtilities.updateComponentTreeUI(f);
 	}
 }
