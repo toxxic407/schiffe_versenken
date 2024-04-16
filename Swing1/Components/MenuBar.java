f439f7c315bd2f68900e61620c214fff4bfac20b
Alejandro Servín <alejandro.sebastian.servin@gmail.com>
2024-04-16 19:25:59 +0200

    Create class for menu bar

---
+++ Swing1/Components/MenuBar.java
@@ -0,0 +1,40 @@
+package Components;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+public class MenuBar extends JMenuBar {
+	
+	public MenuBar(JFrame currentFrame, JFrame menuFrame) 
+	{
+		{
+		    JMenu menu = new JMenu("Menü");
+		    {
+			JMenuItem item = new JMenuItem("Menü öffnen");
+			item.addActionListener(
+			    (e) -> { 
+			    	System.out.println("File -> Menü öffnen"); 
+			    	currentFrame.dispose();    // hide current window
+			    	menuFrame.setVisible(true);   // show Menu window
+			    	
+			    }
+			);
+			menu.add(item);
+		    }
+		    {
+			JMenuItem item = new JMenuItem("Programm beenden");
+			item.addActionListener(
+				    (e) -> { 
+				    	System.out.println("File -> Programm beenden"); 
+				    	System.exit(0);
+				    	}
+				);
+			menu.add(item);
+		    }
+		    this.add(menu);
+		}
+	}	
+	
+}
