8cd828a3737df50b4321b68ae42c6f1f0712d962
toxxic407 <johenpaennle@gmail.com>
2024-06-25 15:51:50 +0200

    Add files via upload

--- Swing1/Components/MenuBar.java
+++ Swing1/Components/MenuBar.java
@@ -1,40 +1,42 @@
 package Components;
 
+import java.net.Socket;
+
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
 public class MenuBar extends JMenuBar {
-	
-	public MenuBar(JFrame currentFrame, JFrame menuFrame) 
-	{
+		
+		public MenuBar(JFrame currentFrame, JFrame menuFrame) 
 		{
-		    JMenu menu = new JMenu("Menü");
-		    {
-			JMenuItem item = new JMenuItem("Menü öffnen");
-			item.addActionListener(
-			    (e) -> { 
-			    	System.out.println("File -> Menü öffnen"); 
-			    	currentFrame.dispose();    // hide current window
-			    	menuFrame.setVisible(true);   // show Menu window
-			    	
-			    }
-			);
-			menu.add(item);
-		    }
-		    {
-			JMenuItem item = new JMenuItem("Programm beenden");
-			item.addActionListener(
+			{
+			    JMenu menu = new JMenu("Menü");
+			    {
+				JMenuItem item = new JMenuItem("Menü öffnen");
+				item.addActionListener(
 				    (e) -> { 
-				    	System.out.println("File -> Programm beenden"); 
-				    	System.exit(0);
-				    	}
+				    	System.out.println("File -> Menü öffnen");
+				    	currentFrame.dispose();    // hide current window
+				    	menuFrame.setVisible(true);   // show Menu window
+				    	
+				    }
 				);
-			menu.add(item);
-		    }
-		    this.add(menu);
-		}
+				menu.add(item);
+			    }
+			    {
+				JMenuItem item = new JMenuItem("Programm beenden");
+				item.addActionListener(
+					    (e) -> { 
+					    	System.out.println("File -> Programm beenden"); 
+					    	System.exit(0);
+					    	}
+					);
+				menu.add(item);
+			    }
+			    this.add(menu);
+			}
 	}	
 	
 }
